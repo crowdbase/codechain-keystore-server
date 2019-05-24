3c4c20cd22441c6e542c1682f08cd24b889350e1
Sangboak Lee <echo3042@gmail.com>
2019-05-24 22:32:29 +0900

    use env variables

--- config/default.js
+++ config/default.js
@@ -4,10 +4,11 @@ module.exports = {
   knex: {
     client: "pg",
     connection: {
-      host: "localhost",
-      port: 5432,
-      database: "codechain-keystore",
-      user: "codechain"
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT || 5432,
+      database: process.env.DB_NAME || "codechain-keystore",
+      user: process.env.DB_USER || "codechain",
+      user: process.env.DB_PASSWORD,
     },
     migrations: {
       directory: path.resolve(__dirname, "..", "migrations"),
